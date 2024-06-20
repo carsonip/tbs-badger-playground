c0d0ba9bb6c7b8bff57bfb88106e829e45c47435
Carson Ip <carson.ip@elastic.co>
2024-06-20 23:18:55 +0100

    Configurable DIR in run.sh

--- prefetch/run.sh
+++ prefetch/run.sh
@@ -2,13 +2,15 @@
 
 set -euo pipefail
 
+DIR=${DIR:-foo}
+
 function run() {
-    echo === ./prefetch run -dir foo -prefetch="$1" -hit="$2"
+    echo === ./prefetch run -dir "$DIR" -prefetch="$1" -hit="$2"
     local NAME="run_prefetch_$1_hit_$2"
     systemctl stop "$NAME".slice
     echo 1 > /proc/sys/vm/drop_caches
     sleep 1
-    systemd-run --scope --slice "$NAME" -p IOAccounting=true time ./prefetch run -dir foo -prefetch="$1" -hit="$2"
+    systemd-run --scope --slice "$NAME" -p IOAccounting=true time ./prefetch run -dir "$DIR" -prefetch="$1" -hit="$2"
     sleep 1
     mkdir -p "$NAME"
     cp -r "/sys/fs/cgroup/$NAME.slice/." "$NAME" 2>/dev/null || true
